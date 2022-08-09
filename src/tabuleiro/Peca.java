cf7163e444fd24a13bccba916543556ebee046bb
Katherine-Martins <katherine.martins@ibm.com>
2022-08-09 16:35:23 -0400

    classes posicao e peca foram criadas

---
+++ src/tabuleiro/Peca.java
@@ -0,0 +1,19 @@
+package tabuleiro;
+
+/* Classe Mãe Peça */
+public abstract class Peca {
+    protected Posicao posicao;
+
+    /* Não deu para entender como declarar esse método
+    public boolean possiveisMovimentos(){
+        //retorna uma matriz de posições
+    } */
+
+    public void posicaoEPossivel (Posicao posicao){
+        //retorna boolean
+    }
+
+    public void algumPossivelMovimento(){
+        //retorna boolean
+    }
+}
